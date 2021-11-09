35f2c2ba227795b7dbc0307115fad4cec65b1672
Lola Adewale <dia7@duke.edu>
2021-11-09 18:02:57 -0500

    loljk

--- src/PercolationDFSFast.java
+++ src/PercolationDFSFast.java
@@ -9,6 +9,6 @@ public class PercolationDFSFast extends PercolationDFS{
     }
     @Override
     protected void updateOnOpen(int row, int col){
-    if (row == 0 || isFull(row - 1, col)||col != 0 && isFull(row, col - 1)||(col < myGrid.length - 1 && isFull(row, col + 1))){dfs(row,col);}
+    if (row == 0 || isFull(row - 1, col)||col != 0 && isFull(row, col - 1)||(col < myGrid.length - 1 && isFull(row, col + 1)) ||row < myGrid.length - 1 && isFull(row + 1, col)){dfs(row,col);}
     }
 }
